b1eaa3215f7aefdc8bf89b81051fe814d549d88b
SimWonKim <asdfg4421@gmail.com>
2020-03-15 03:22:09 +0900

    nest js 튜토리얼 2일차 커밋. (controller 까지 완료.)

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,17 +1,49 @@
-import { Controller, Get, Req, Param } from '@nestjs/common';
+import { Controller, Get, Post, Req, Param, Body, HttpCode, Put, Query, Bind } from '@nestjs/common';
 import { Request } from 'express';
+import { CreateCatDto } from './dto/create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
 
+    @Post()
+    async create(@Body() createCatDto : CreateCatDto) {
+        return createCatDto;
+    }
+
     @Get()
-    findAll(@Req() request : Request) : string {
+    @HttpCode(200)
+    findCats(@Query('name') name) {
+        return { name }
+    }
+
+    @Get()
+    findAll(@Req() request: Request): string {
         console.log(request);
         return 'This action returns all cats';
     }
 
+    // @Get(':id')
+    // find(@Param('id') id) {
+    //     return id;
+    // }
+
     @Get(':id')
-    find(@Param('id') id) {
-        return id;
+    @Bind(Param())
+    findOne(params) {
+        return params.id;
     }
+
+    // @Post()
+    // @HttpCode(201)
+    // createCats(@Body('name') name) {
+    //     return name;
+    // }
+
+  
+
+    @Put()
+    @HttpCode(200)
+    updateCats(@Body('id') id, @Body('name') name) {
+        return { id, name };
+    } 
 }
