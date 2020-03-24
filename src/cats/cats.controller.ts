5d4d4087db6e83394055577a28755c0e80a2df44
KimSimWon <asdfg4421@gmail.com>
2020-03-25 00:20:50 +0900

    Provider 까지 진행.

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,49 +1,65 @@
-import { Controller, Get, Post, Req, Param, Body, HttpCode, Put, Query, Bind } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Get, Post, Body, HttpCode } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
+import { CatsService } from './cats.service';
+import { Cat } from './interface/cats.interface';
 
 @Controller('cats')
 export class CatsController {
+    constructor(private catsService: CatsService) { }
 
     @Post()
+    @HttpCode(201)
     async create(@Body() createCatDto : CreateCatDto) {
-        return createCatDto;
+        this.catsService.create(createCatDto);
     }
 
     @Get()
     @HttpCode(200)
-    findCats(@Query('name') name) {
-        return { name }
+    async findAll(): Promise<Cat[]> {
+        return this.catsService.findAll();
     }
 
-    @Get()
-    findAll(@Req() request: Request): string {
-        console.log(request);
-        return 'This action returns all cats';
-    }
 
-    // @Get(':id')
-    // find(@Param('id') id) {
-    //     return id;
-    // }
-
-    @Get(':id')
-    @Bind(Param())
-    findOne(params) {
-        return params.id;
-    }
 
     // @Post()
-    // @HttpCode(201)
-    // createCats(@Body('name') name) {
-    //     return name;
+    // async create(@Body() createCatDto : CreateCatDto) {
+    //     return createCatDto;
     // }
 
-  
+    // @Get()
+    // @HttpCode(200)
+    // findCats(@Query('name') name) {
+    //     return { name }
+    // }
 
-    @Put()
-    @HttpCode(200)
-    updateCats(@Body('id') id, @Body('name') name) {
-        return { id, name };
-    } 
+    // @Get()
+    // findAll(@Req() request: Request): string {
+    //     console.log(request);
+    //     return 'This action returns all cats';
+    // }
+
+    // // @Get(':id')
+    // // find(@Param('id') id) {
+    // //     return id;
+    // // }
+
+    // @Get(':id')
+    // @Bind(Param())
+    // findOne(params) {
+    //     return params.id;
+    // }
+
+    // // @Post()
+    // // @HttpCode(201)
+    // // createCats(@Body('name') name) {
+    // //     return name;
+    // // }
+
+
+
+    // @Put()
+    // @HttpCode(200)
+    // updateCats(@Body('id') id, @Body('name') name) {
+    //     return { id, name };
+    // } 
 }
